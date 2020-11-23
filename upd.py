f0c9c3a9f7e2451c9c217de52983f0a7d58823c9
Yifan Wu <shinbokuow@163.com>
2020-11-23 20:22:24 +0800

    Update read-tock time.struct_time(tm_year=2020, tm_mon=11, tm_mday=23, tm_hour=20, tm_min=22, tm_sec=24, tm_wday=0, tm_yday=328, tm_isdst=0)

---
+++ upd.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+import os
+import sys
+import time
+
+message = ''
+if len(sys.argv) == 1:
+    message = 'Update'
+else:
+    message = sys.argv[1]
+pattern = "git add -A && git commit -m \"Update %s %s\" && git push"
+os.system(pattern % (message, time.localtime(time.time())))
